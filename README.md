74116d32d026ad872fd2dbce80d6990e3d7de5d7
mattia richetto <mrichetto@opentable.com>
2016-11-29 00:28:19 -0800

    add camera management

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # blue-dawg
 
 blue-dawg
+
+## How to run the mobile-app
+
+- Launch Exponent XDE
+
+- Project > Open Project (e.g.: /Users/mrichetto/projects/blue-dawg/mobile-app/blue-dawg-v0)
--- mobile-app/blue-dawg-v0/main.js
+++ mobile-app/blue-dawg-v0/main.js
@@ -5,8 +5,11 @@ import {
   Text,
   View,
   Button,
+  Image,
   Alert
 } from 'react-native';
+
+import cameraIcon from './camera-2-xxl.png';
 
 const styles = StyleSheet.create({
   container: {
@@ -32,25 +35,44 @@ const styles = StyleSheet.create({
   }
 });
 
+const beginning = 'And the answer is ...';
+
 class App extends React.Component {
-  render() {
-    const onPressCheck = () => {
-      let answer = 'Maybe';
-      const number = Math.round(Math.random() * 3); // 0, 1, 2, 3
-      switch (number) {
-        case 1:
-          answer = 'Yes';
-          break;
-        case 2:
-          answer = 'No';
-          break;
-        default:
-          break;
-      }
+  constructor(props) {
+    super(props);
 
-      Alert.alert(`And the answer is ...\n**${answer}**`);
+    this.state = {
+      answer: beginning,
+      cameraIcon,
+      photoTaken: false
     };
+  }
 
+  onPressLaunchCamera = async () => {
+    const data = await Exponent.ImagePicker.launchCameraAsync({});
+    if (!data.cancelled) {
+      this.setState({ cameraIcon: { uri: data.uri }, photoTaken: true });
+    }
+  }
+
+  onPressCheck = () => {
+    let answer = 'Maybe';
+    const number = Math.round(Math.random() * 3); // 0, 1, 2, 3
+    switch (number) {
+      case 1:
+        answer = 'Yes';
+        break;
+      case 2:
+        answer = 'No';
+        break;
+      default:
+        break;
+    }
+
+    this.setState({ answer: `${beginning} **${answer}**`, photoTaken: false });
+  };
+
+  render() {
     return (
       <View style={styles.container}>
         <View style={{ height: 20, justifyContent: 'center' }}>
@@ -60,16 +82,35 @@ class App extends React.Component {
             blue-dawg
           </Text>
         </View>
-        <View style={{ flex: 1, backgroundColor: 'skyblue', justifyContent: 'center' }}>
+        <View style={{ flex: 1, backgroundColor: 'skyblue', justifyContent: 'center', alignItems: 'center' }}>
           <Text style={styles.logo}>
             ＵＴｪＴＵ
           </Text>
+          <Image
+            source={this.state.cameraIcon}
+            style={{
+              width: 250,
+              height: 250,
+              borderColor: 'white',
+              borderWidth: 5,
+              marginTop: 25,
+              marginBottom: 25
+            }} />
           <Button
-            onPress={onPressCheck}
+            disabled={this.state.photoTaken}
+            onPress={this.onPressLaunchCamera}
+            title="Launch Camera"
+            color="black"
+            accessibilityLabel="Launch Camera"
+            />
+          <Button
+            disabled={!this.state.photoTaken}
+            onPress={this.onPressCheck}
             title="Check"
             color="black"
             accessibilityLabel="Check"
             />
+          <Text style={{ fontSize: 18 }}>{this.state.answer}</Text>
         </View>
         <View style={{ height: 49, backgroundColor: 'steelblue', justifyContent: 'center' }}>
           <Text style={styles.small}>
